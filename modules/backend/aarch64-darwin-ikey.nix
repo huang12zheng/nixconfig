12b595075ea19ac8ee259e4c2d4bd4947a05ae9c
hzgood <805104533@qq.com>
2026-05-31 02:35:54 +0800

    feat(backend): 支持通过配置设置Cargo仓库镜像和调整构建参数
    - 在模块参数中新增cfg字段以继承cargo配置中的CONFIG_REGISTRY
    - 允许通过配置动态设置.cargo/config.toml中crates-io的索引地址
    - 针对aarch64-apple-darwin目标添加rust编译链接参数以解决构建问题
    - 注释禁用binstalls中的部分工具以应对网络连接问题
    - 优化依赖包列表，删除部分注释和未使用项

--- modules/backend/aarch64-darwin-ikey.nix
+++ modules/backend/aarch64-darwin-ikey.nix
@@ -1,7 +1,8 @@
-{ pkgs, lib, username,... }:
+{ pkgs, cfg, lib, username,... }:
 
 let
   useChinaMirror = true;
+  inherit (cfg.cargo) CONFIG_REGISTRY;
 in {
   home-manager.users.${username} = {
     home.packages = with pkgs; [
@@ -22,23 +23,26 @@ in {
       GCC_LIB = "${pkgs.gcc-unwrapped.lib}/lib";
       OSO_URL = "http://localhost:3000";
       OSO_AUTH = "e_0123456789_12345_osotesttoken01xiIn";
+      # LIBRARY_PATH = "${LIBRARY_PATH:+$LIBRARY_PATH:}:$(xcrun --show-sdk-path)/usr/lib";
+      # binstalls 暂时禁用，网络连接问题
       binstalls = lib.strings.concatStringsSep " " [
         "flutter_rust_bridge_codegen"
         "cargo-insta"
         "cargo-cache"
-        #"rust"
         "cargo-component"
-        "twiggy"
         "cargo-generate"
-        "cargo-clean-all"
         "wit-bindgen-cli"
+        #"cargo-clean-all"
       ];
     };
 
     home.file = lib.mkIf useChinaMirror {
       ".cargo/config.toml".text = ''
         [registries.crates-io]
-        index = "sparse+https://mirrors.ustc.edu.cn/crates.io-index/"
+        index = "${CONFIG_REGISTRY}"
+        # 为什么加上就可以编译了?
+        [target.aarch64-apple-darwin]
+        rustflags = [ "-C", "link-arg=-undefined", "-C", "link-arg=dynamic_lookup", ]
       '';
     };
   };
